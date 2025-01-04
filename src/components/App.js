423ed69bab6303270909b16cfa7631cd871c3f92
RitankJaikar <ritank.jaikar@gmail.com>
2025-01-04 19:54:59 +0530

    Submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import Child from "./Child";
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      task: "Learn React",
+      isComplete: false
+    },
+    {
+      task: "Build React App",
+      isComplete: false
+    },
+    {
+      task: "Deploy the React app",
+      isComplete: false
+    }
+  ]);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <div>Parent Component</div>
+        <Child todos={todos} setTodos={setTodos} />
     </div>
   )
 }
