d50eafd638f493cf068fb8e83f32177ad7049de4
RitankJaikar <ritank.jaikar@gmail.com>
2025-01-04 19:55:05 +0530

    Submit

---
+++ src/components/Child.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function Child({todos, setTodos}) {
+    function handleComplete(index) {
+        let updatedTodos = todos.map((todo, i) => {
+            if(i === index) {
+                return {...todo, isComplete: !todo.isComplete}
+            }
+            return todo;
+        })
+        setTodos(updatedTodos);
+    }
+
+    return (
+        <div>
+            <div>Child Component</div>
+            <ul>
+                {
+                    todos.map((todo, i) => {
+                        return (
+                            <li key={i}>
+                                <span>{todo.task}</span>
+                                {!todo.isComplete && <button onClick={() => handleComplete(i)}>Complete</button>}
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </div>
+    )
+}
